b3da6527c0ef8928a5a547014abecf352eaed118
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-07-31 12:26:53 +0200

    Project started

---
+++ src/test/java/com/danimartinezmarquez/icedlatteproject/IcedlatteprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danimartinezmarquez.icedlatteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcedlatteprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
