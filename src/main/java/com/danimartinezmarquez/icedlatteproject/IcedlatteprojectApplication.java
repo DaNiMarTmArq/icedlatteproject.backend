18db8a8358e6b51be59b5e0f4faaafe9bdfa8262
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-07-31 12:27:50 +0200

    Remove unused imports

--- src/main/java/com/danimartinezmarquez/icedlatteproject/IcedlatteprojectApplication.java
+++ src/main/java/com/danimartinezmarquez/icedlatteproject/IcedlatteprojectApplication.java
@@ -1,9 +1,8 @@
 package com.danimartinezmarquez.icedlatteproject;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class IcedlatteprojectApplication {
